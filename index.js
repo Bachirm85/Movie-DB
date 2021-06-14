e881556b8e4938779b5821e062f12f5703378c02
Bachirm85 <mouawadbachir@gmail.com>
2021-06-14 15:53:11 +0300

    step 5

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.get('/hello/:id', (req, res) => {
     res.send({status:200, message:'Hello,' + req.params.id})
   })
 
-  app.get('/hello/', (req, res) => {
+app.get('/hello/', (req, res) => {
     res.send({status:200, message:'Hello, User'})
   })
 
-  app.get('/search', (req, res) => {
-      const data = req.query.search;
+app.get('/search', (req, res) => {
+    const data = req.query.search;
     if (data)
     res.send({
         status:200, 
@@ -38,7 +38,32 @@ app.get('/hello/:id', (req, res) => {
         message:"you have to provide a search", 
         });
 
+})
+
+
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.get('/movies/create', (req, res) => {  
+    
   })
+
+app.get('/movies/read', (req, res) => {  
+    res.send({status:200, data:movies })
+})
+
+app.get('/movies/update', (req, res) => {  
+})
+
+app.get('/movies/delete', (req, res) => {  
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
